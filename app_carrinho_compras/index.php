670910886f3d8e91f31bcf6d8cc64eb1d9e61171
Laysa Fontoura <laysafontoura@gmail.com>
2023-04-02 18:19:40 -0300

    SRP

--- app_carrinho_compras/index.php
+++ app_carrinho_compras/index.php
@@ -3,25 +3,55 @@
 require __DIR__."/vendor/autoload.php";
 
 use src\CarrinhoCompra;
+use src\Item;
+use src\Pedido;
+use src\EmailService;
 
-$carrinho1 = new CarrinhoCompra();
+$pedido = new Pedido();
+$item1 = new Item();
+$item2 = new Item();
 
-print_r($carrinho1->exibirItens());
-$carrinho1->confirmarPedido();
-echo 'Valor total: '.$carrinho1->exibirValorTotal();
-echo '<br/>';
-echo 'status: '.$carrinho1->exibeStatus();
+$item1->setDescricao('Porta');
+$item1->setValor(1.99);
 
-echo '<br/>';
+$item2->setDescricao('Janela');
+$item2->setValor(2.99);
 
-$carrinho1->addItens('Cadeira', 100);
-$carrinho1->addItens('Cadeira', 100);
-$carrinho1->addItens('Cadeira', 100);
+$pedido->getCarrinhoCompra()->addItens($item1);
+$pedido->getCarrinhoCompra()->addItens($item2);
 
-echo '<br/>';
-print_r($carrinho1->exibirItens());
-echo 'Valor total: '.$carrinho1->exibirValorTotal();
+echo '<h4>Pedido</h4>';
+echo '<pre>';
+print_r($pedido);
+echo '</pre>';
 
-$carrinho1->confirmarPedido();
-echo '<br/>';
-echo 'status: '.$carrinho1->exibeStatus();
+echo '<h4>Itens do carrinho</h4>';
+echo '<pre>';
+print_r($pedido->getCarrinhoCompra()->getItens());
+echo '</pre>';
+
+echo '<h4>Valor do pedido</h4>';
+
+$total = 0;
+foreach($pedido->getCarrinhoCompra()->getItens() as $key => $item){
+  $total += $item->getValor();
+}
+echo $total;
+
+echo '<h4>Carrinho valido?</h4>';
+print_r($pedido->getCarrinhoCompra()->validarCarrinho());
+
+echo '<h4>Status do Pedido</h4>';
+print_r($pedido->getStatus());
+
+echo '<h4>Confirmar Pedido</h4>';
+print_r($pedido->confirmar());
+
+echo '<h4>Status do Pedido</h4>';
+print_r($pedido->getStatus());
+
+echo '<h4>E-mail</h4>';
+if($pedido->getStatus() == 'confirmado'){
+  echo EmailService::dispararEmail();
+}
+
--- app_carrinho_compras/src/CarrinhoCompra.php
+++ app_carrinho_compras/src/CarrinhoCompra.php
@@ -1,47 +1,29 @@
 <?php
 namespace src;
-
+use src\Item;
 class CarrinhoCompra{
     
     private $itens;
-    private $status;
-    private $valorTotal;
 
     public function __construct(){
         $this->itens = [];
-        $this->status = 'aberto';
-        $this->valorTotal = 0;
     }
+
+    public function getItens(){
+        return $this->itens;
+    } 
 
     public function exibirItens(){
         return $this->itens;
     }
 
-    public function addItens(string $item, float $valor){
-        array_push($this->itens, ["Item: " => $item, "Valor: " => $valor]);
-        $this->valorTotal += $valor;
+    public function addItens(Item $item){
+        array_push($this->itens, $item);
         return true;
     }
 
-    public function exibirValorTotal(){
-        return $this->valorTotal;
-    }
-
-    public function exibeStatus(){
-        return $this->status;
-    }
-
-    public function confirmarPedido(){
-        if ($this->itens != null) {
-            $this->status = 'Confirmado';
-            $this->enviarEmailConfirmado( );
-        }else{  
-            echo '<br/> Nenhum item no carrinho.<br/>';  
-        }
-    }
-
-    public function enviarEmailConfirmado(){
-        echo '<br/>... envia email de confirmação ...<br/>'; 
+    public function validarCarrinho(){
+        return count($this->itens) > 0;
     }
 
 }
