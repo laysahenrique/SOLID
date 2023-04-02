fb325839cbe866342eb11af142d08a02c547237a
Laysa Fontoura <laysafontoura@gmail.com>
2023-04-02 15:58:26 -0300

    Antes de aplicar SOLID

--- app_carrinho_compras/index.php
+++ app_carrinho_compras/index.php
@@ -7,7 +7,21 @@ use src\CarrinhoCompra;
 $carrinho1 = new CarrinhoCompra();
 
 print_r($carrinho1->exibirItens());
+$carrinho1->confirmarPedido();
+echo 'Valor total: '.$carrinho1->exibirValorTotal();
+echo '<br/>';
+echo 'status: '.$carrinho1->exibeStatus();
+
+echo '<br/>';
 
 $carrinho1->addItens('Cadeira', 100);
+$carrinho1->addItens('Cadeira', 100);
+$carrinho1->addItens('Cadeira', 100);
 
+echo '<br/>';
 print_r($carrinho1->exibirItens());
+echo 'Valor total: '.$carrinho1->exibirValorTotal();
+
+$carrinho1->confirmarPedido();
+echo '<br/>';
+echo 'status: '.$carrinho1->exibeStatus();
--- app_carrinho_compras/src/CarrinhoCompra.php
+++ app_carrinho_compras/src/CarrinhoCompra.php
@@ -19,6 +19,29 @@ class CarrinhoCompra{
 
     public function addItens(string $item, float $valor){
         array_push($this->itens, ["Item: " => $item, "Valor: " => $valor]);
+        $this->valorTotal += $valor;
+        return true;
+    }
+
+    public function exibirValorTotal(){
+        return $this->valorTotal;
+    }
+
+    public function exibeStatus(){
+        return $this->status;
+    }
+
+    public function confirmarPedido(){
+        if ($this->itens != null) {
+            $this->status = 'Confirmado';
+            $this->enviarEmailConfirmado( );
+        }else{  
+            echo '<br/> Nenhum item no carrinho.<br/>';  
+        }
+    }
+
+    public function enviarEmailConfirmado(){
+        echo '<br/>... envia email de confirmação ...<br/>'; 
     }
 
 }
